307ead7a61770d76edca1a130485f7dbe01a65ef
Tomos Corbin <tom_corbin1@hotmail.com>
2025-11-19 10:16:51 +0000

    build.clj updated

--- build.clj
+++ build.clj
@@ -1,39 +1,38 @@
 (ns build
-  (:refer-clojure :exclude [test])
   (:require [clojure.tools.build.api :as b]))
 
 (def lib 'io.github.tomoscorbin/blueprint)
-(def version "0.1.0-SNAPSHOT")
-(def main 'tomoscorbin.blueprint)
+(def version "0.1.0")
+
+(def main 'blueprint.core)           ;; make sure this ns exists and has -main
+
 (def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
 
-(defn test "Run all the tests." [opts]
-  (let [basis    (b/create-basis {:aliases [:test]})
-        cmds     (b/java-command
-                  {:basis     basis
-                   :main      'clojure.main
-                   :main-args ["-m" "cognitect.test-runner"]})
-        {:keys [exit]} (b/process cmds)]
-    (when-not (zero? exit) (throw (ex-info "Tests failed" {}))))
-  opts)
+(def uber-file (format "target/%s-%s-standalone.jar"
+                       (name lib)
+                       version))
 
-(defn- uber-opts [opts]
-  (assoc opts
-         :lib lib :main main
-         :uber-file (format "target/%s-%s.jar" lib version)
-         :basis (b/create-basis {})
-         :class-dir class-dir
-         :src-dirs ["src"]
-         :ns-compile [main]))
+(defn clean [_]
+  (b/delete {:path "target"}))
 
-(defn ci "Run the CI pipeline of tests (and build the uberjar)." [opts]
-  (test opts)
-  (b/delete {:path "target"})
-  (let [opts (uber-opts opts)]
-    (println "\nCopying source...")
-    (b/copy-dir {:src-dirs ["resources" "src"] :target-dir class-dir})
-    (println (str "\nCompiling " main "..."))
-    (b/compile-clj opts)
-    (println "\nBuilding JAR..." (:uber-file opts))
-    (b/uber opts))
-  opts)
+(defn uber [_]
+  ;; clean build dir
+  (clean nil)
+
+  ;; copy sources/resources
+  (b/copy-dir {:src-dirs  ["src" "resources"]
+               :target-dir class-dir})
+
+  ;; compile Clojure to classes
+  (b/compile-clj {:basis     basis
+                  :src-dirs  ["src"]
+                  :class-dir class-dir})
+
+  ;; build the uberjar
+  (b/uber {:class-dir class-dir
+           :uber-file uber-file
+           :basis     basis
+           :main      main
+           :lib       lib})
+  (println "Built" uber-file))
